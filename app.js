012e44626eedfcc9c3c7e07c50bf3ac920297e14
Julio <almiror.info@gmail.com>
2023-03-04 12:10:47 -0500

    ❤️❤️

--- app.js
+++ app.js
@@ -80,8 +80,12 @@ buttons.addEventListener('click', e => {
                 } else {
                     result.innerText += e.target.innerText                 
                 }  
-            } else {                
-                result.innerText += e.target.innerText                  
+            } else {                 
+                if(isNaN(compareOperations) && compareOperations === val){
+                    result.innerText = result.innerText.slice(0, result.innerText.length - 1) + e.target.innerText    
+                } else {                    
+                result.innerText += e.target.innerText  
+                }      
             }
         }
         // si el target tiene el valoro igual generamos el valor de las operaciones
